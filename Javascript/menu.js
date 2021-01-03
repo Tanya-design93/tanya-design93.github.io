1686fa8bc8a53e9aa8e8531d6b6a7a54304d24e2
Tanya-design93 <67875354+Tanya-design93@users.noreply.github.com>
2021-01-02 21:42:44 -0400

    Update menu.js

--- Javascript/menu.js
+++ Javascript/menu.js
@@ -49,9 +49,9 @@ function addElement(parentContainer, elementId, descriptionText) {
   var column = CreateColumn();
 
   column.appendChild(CreateImageElement(elementId));
-  column.appendChild(CreateColumnDescriptionText(descriptionText.split("$")));
-  column.appendChild(CreateAddToCartButton(descriptionText.split("$")[0]));
-  column.id = (descriptionText.split("$")[0].trim());
+  //column.appendChild(CreateColumnDescriptionText(descriptionText.split("$")));
+  //column.appendChild(CreateAddToCartButton(descriptionText.split("$")[0]));
+  //column.id = (descriptionText.split("$")[0].trim());
   parentContainer.appendChild(column);
 }
 
